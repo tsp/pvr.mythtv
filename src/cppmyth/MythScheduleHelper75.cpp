72d3b20eb2b0b2b946ac4769bb8be3b8607402c6
janbar <jlbarriere68@gmail.com>
2015-06-30 17:19:55 +0200

    Add timer type TIMER_TYPE_SEARCH_KEYWORD

--- src/cppmyth/MythScheduleHelper75.cpp
+++ src/cppmyth/MythScheduleHelper75.cpp
@@ -186,6 +186,26 @@ const std::vector<MythScheduleManager::TimerType>& MythScheduleHelper75::GetTime
             &GetRuleRecordingGroupList(),
             GetRuleRecordingGroupDefault()));
 
+    typeList.push_back(MythScheduleManager::TimerType(TIMER_TYPE_SEARCH_KEYWORD,
+            PVR_TIMER_TYPE_IS_REPEATING |
+            PVR_TIMER_TYPE_SUPPORTS_ENABLE_DISABLE |
+            PVR_TIMER_TYPE_SUPPORTS_TITLE_EPG_MATCH |
+            PVR_TIMER_TYPE_SUPPORTS_CHANNELS |
+            PVR_TIMER_TYPE_SUPPORTS_RECORD_ONLY_NEW_EPISODES |
+            PVR_TIMER_TYPE_SUPPORTS_START_END_MARGIN |
+            PVR_TIMER_TYPE_SUPPORTS_PRIORITY |
+            PVR_TIMER_TYPE_SUPPORTS_LIFETIME |
+            PVR_TIMER_TYPE_SUPPORTS_RECORDING_GROUP,
+            XBMC->GetLocalizedString(30467),
+            &GetRulePriorityList(),
+            GetRulePriorityDefault(),
+            &GetRuleDupMethodList(),
+            GetRuleDupMethodDefault(),
+            &GetRuleExpirationList(),
+            GetRuleExpirationDefault(),
+            &GetRuleRecordingGroupList(),
+            GetRuleRecordingGroupDefault()));
+
     ///////////////////////////////////////////////////////////////////////////
     //// KEEP LAST
     ///////////////////////////////////////////////////////////////////////////
@@ -529,6 +549,9 @@ bool MythScheduleHelper75::FillTimerEntryWithRule(MythTimerEntry& entry, const M
       entry.epgSearch = rule.Description();
       break;
     case Myth::ST_KeywordSearch:
+      entry.epgSearch = rule.Description();
+      entry.timerType = TIMER_TYPE_SEARCH_KEYWORD;
+      break;
     case Myth::ST_PeopleSearch:
     case Myth::ST_PowerSearch:
       entry.epgSearch = rule.Description();
@@ -556,6 +579,7 @@ bool MythScheduleHelper75::FillTimerEntryWithRule(MythTimerEntry& entry, const M
     case TIMER_TYPE_RECORD_DAILY:
     case TIMER_TYPE_RECORD_ALL:
     case TIMER_TYPE_RECORD_SERIES:
+    case TIMER_TYPE_SEARCH_KEYWORD:
     case TIMER_TYPE_UNHANDLED:
       if (difftime(rule.NextRecording(), 0) > 0)
       {
@@ -1076,6 +1100,29 @@ MythRecordingRule MythScheduleHelper75::NewFromTimer(const MythTimerEntry& entry
         rule.SetCategory(entry.epgInfo.Category());
         rule.SetProgramID(entry.epgInfo.ProgramID());
         rule.SetSeriesID(entry.epgInfo.SeriesID());
+        rule.SetInactive(entry.isInactive);
+        return rule;
+      }
+      break;
+    }
+
+    case TIMER_TYPE_SEARCH_KEYWORD:
+    {
+      if (!entry.epgSearch.empty())
+      {
+        if (entry.HasChannel())
+        {
+          rule.SetType(Myth::RT_ChannelRecord);
+          rule.SetChannelID(entry.chanid);
+          rule.SetCallsign(entry.callsign);
+        }
+        else
+          rule.SetType(Myth::RT_AllRecord);
+        rule.SetSearchType(Myth::ST_KeywordSearch); // Search keyword
+        rule.SetTitle(entry.title);
+        // Backend use the subtitle/description to find program by keywords or title
+        rule.SetSubtitle("");
+        rule.SetDescription(entry.epgSearch);
         rule.SetInactive(entry.isInactive);
         return rule;
       }
